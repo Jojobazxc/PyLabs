506c5b4c0cb784082b11a916bbf00c2da6ca9cd5
Jojoba <piperov2004@mail.ru>
2024-09-06 14:20:37 +0300

    add Lab1

---
+++ Пиперов/Labs/Lab1.py
@@ -0,0 +1,28 @@
+from random import randint
+
+#Написать функцию, которая принимает два
+#целочисленных списка и возвращает True, если
+#первые или последние элементы данных списков равны.
+#Оба списка содержат 1 или более элементов
+def is_last_equal(first_list: list[int], second_list: list[int]):
+    if first_list[-1] == second_list[-1] or first_list[0] == second_list[0]:
+        return True
+    else:
+        return False
+
+
+result = False
+while not result:
+    length_of_list = int(input("Введите длину списка: "))
+    list1 = []
+    list2 = []
+
+    for i in range(length_of_list):
+        list1.append(randint(0, 10))
+        list2.append(randint(0, 10))
+
+    result = is_last_equal(list1, list2)
+    print(f"Первый список: {list1}")
+    print(f"Второй список: {list2}")
+    print(result)
+
